6ead44f92d1498174640c2fbc7594bcc02bb2636
socrates <shauryayamdagni0@gmail.com>
2024-11-04 16:25:34 +0530

    cart added

--- src/main/java/com/indiafirstpandit/model/Cart.java
+++ src/main/java/com/indiafirstpandit/model/Cart.java
@@ -1,10 +1,8 @@
 package com.indiafirstpandit.model;
-
 import jakarta.persistence.*;
 
 import java.util.List;
 import java.util.UUID;
-
 @Entity
 public class Cart {
 
@@ -15,10 +13,10 @@ public class Cart {
     @OneToMany
     private List<Puja> puja ;
 
-    @OneToOne
-    private User user;
-
     @OneToMany
     private List<Product> product;
+
+    @OneToMany
+    private List<Samagri> samagri;
 
 }
