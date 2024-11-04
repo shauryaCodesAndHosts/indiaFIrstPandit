4986d3483d345e37226ad9c0cb6c5b7b1e59d0a0
socrates <shauryayamdagni0@gmail.com>
2024-11-04 17:04:12 +0530

    cart added

--- src/main/java/com/indiafirstpandit/service/CartService.java
+++ src/main/java/com/indiafirstpandit/service/CartService.java
@@ -1,4 +1,37 @@
 package com.indiafirstpandit.service;
 
+import com.indiafirstpandit.model.Cart;
+import com.indiafirstpandit.model.Product;
+import com.indiafirstpandit.model.Puja;
+import com.indiafirstpandit.model.Samagri;
+import com.indiafirstpandit.repo.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class CartService {
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    public List<Puja> getAllPujas(UUID id)
+    {
+        Cart cart= cartRepository.findById(id).get();
+        return cart.getPuja();
+    }
+
+    public List<Product> getAllProducts(UUID id)
+    {
+        return cartRepository.findById(id).get().getProduct();
+    }
+
+    public List<Samagri> getAllSamagri(UUID id)
+    {
+        return cartRepository.findById(id).get().getSamagri();
+    }
+
+
 }
